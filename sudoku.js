8bbde689108ca196ef71b0f7ec506ea1b21dd459
waifan321 <waileungfan@gmail.com>
2024-10-15 23:48:15 +0100

    start

---
+++ sudoku.js
@@ -0,0 +1,64 @@
+class Sudoku {
+    constructor(board) {
+        this.board = board;
+    }
+
+    isValid(board, row, col, num) {
+        for (let x = 0; x < 9; x++) {
+            if (board[row][x] === num || board[x][col] === num || 
+                board[3 * Math.floor(row / 3) + Math.floor(x / 3)][3 * Math.floor(col / 3) + x % 3] === num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    solveSudoku(board) {
+        for (let row = 0; row < 9; row++) {
+            for (let col = 0; col < 9; col++) {
+                if (board[row][col] === 0) {
+                    for (let num = 1; num <= 9; num++) {
+                        if (this.isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (this.solveSudoku(board)) {
+                                return true;
+                            }
+                            board[row][col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    printBoard(board) {
+        for (let row = 0; row < 9; row++) {
+            let line = "";
+            for (let col = 0; col < 9; col++) {
+                line += board[row][col] + " ";
+            }
+            console.log(line);
+        }
+    }
+}
+
+const board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+];
+
+const sudoku = new Sudoku(board);
+if (sudoku.solveSudoku(board)) {
+    sudoku.printBoard(board);
+} else {
+    console.log("No solution exists");
+}
